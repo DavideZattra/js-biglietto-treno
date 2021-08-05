d4bc7be7d5d91ac1b8015f649ae184427f481475
DavideZattra <d.zattra@gmail.com>
2021-08-05 22:17:09 +0200

    finalized exercise js-biglietto-treno

--- js/script.js
+++ js/script.js
@@ -1,28 +1,68 @@
-//  a1 - inserire prompt per definire i km che si vogliono percorrere
-//  a2 - inserire prompt per definire l'età del passeggero
-
-//  b1 - definire le variabili necessarie al calcolo del prezzo intero del biglietto
 
 
 
-let travelDistance = prompt("Quanti Km vorresti percorrere?");
-let passengerAge = prompt("Quanti anni hai?");
+// Distance user input
+let travelDistance = parseInt (prompt ("Inserisci i Km vorresti percorrere?") );
+
+if (isNaN (travelDistance) ) {
+    // if the distance is not given in number
+    alert("Inserisci una distanza valida in km");
+
+} else if (travelDistance <= 0){
+
+    // if the distance is negative
+    alert("Inserisci una distanza valida in km");
+
+}
+
+// age user input
+let passengerAge = parseInt (prompt ("Quanti anni hai?") );
+
+if (isNaN (passengerAge) ) {
+
+    // If the age is not given in number
+    alert("Inserisci un età valida");
+
+} else if ( (passengerAge < 0) && (passengerAge > 120) ) {
+
+    // if the age is not between 0 and 120 
+    alert("Inserisci un età valida compresa tra 0 e 120 anni");
+
+}
+
+// price per km const
+const pricePerKm = 0.21;
+
+// formula for ticket price before discounts
+let ticketFullPrice = travelDistance * pricePerKm;
+
+// Elderly discount value 
+const elderlyDiscount = 0.4;
+
+// youth discount value
+const youthDiscount = 0.2;
+
+// ticket price variable defined
 let ticketPrice;
-let pricePerKm = 0.21;
-let elderlyDiscount = 0.4;
-let youthDiscount = 0.2;
 
 if (passengerAge >= 65) {
 
-    ticketPrice = travelDistance * pricePerKm * elderlyDiscount;
+    // if the passenger is over 65 years
+    ticketPrice = ticketFullPrice - (ticketFullPrice * elderlyDiscount);
 
-} else if (passengerAge <=18) {
+} else if (passengerAge <18) {
 
+    // if the passenger is under 18 years
+    ticketPrice = ticketFullPrice - (ticketFullPrice * youthDiscount);   
 
-    ticketPrice = travelDistance * pricePerKm * youthDiscount;
-    
+} else {
+
+    // if the passenger age is between 18 years and 65
+    ticketPrice = ticketFullPrice;
+
 }
-// let ticketFullPrice = pricePerKm * travelDistance;
 
-document.getElementById("ms_ticket-price"). innerHTML = ticketPrice;
+// to display in the html the final price of ticket 
+// (toFixed(2) used for cut decimals after 2 of them)
+document.getElementById("ms_ticket-price"). innerHTML = ticketPrice.toFixed(2) + "&euro;";
  
